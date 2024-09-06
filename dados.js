1246e42ae0554e491fd6e4a1f3cbcb4104e00ac9
Tayni Schukes <taycschukes@gmail.com>
2024-09-05 23:34:19 -0300

    Alterações até a terceira aula

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo : "William Shakespeare",
+        descricao : "O dramaturgo inglês é considerado o autor com o maior número de livros vendidos de todos os tempos, com estimativas que variam entre 2 e 4 bilhões de cópias.",
+        link : "https://pt.wikipedia.org/wiki/William_Shakespeare"
+    },
+    {
+        titulo: "J.K. Rowling",
+        descricao: "Escritora britânica, mais conhecida por criar o universo de Harry Potter. Suas obras são um fenômeno mundial e inspiraram filmes, peças de teatro e diversos produtos.",
+        link: "https://pt.wikipedia.org/wiki/J._K._Rowling"
+    },
+    {
+        titulo: "Dan Brown",
+        descricao: "Autor de best-sellers, Dan Brown é conhecido por suas tramas intrigantes e mistérios que envolvem história, religião e ciência. Seus livros são traduzidos para diversos idiomas e adaptados para o cinema.",
+        link: "https://pt.wikipedia.org/wiki/Dan_Brown"
+    },
+    {
+        titulo: "Agatha Christie",
+        descricao: "Escritora britânica, considerada a rainha do crime. Conhecida por suas histórias de detetives como Hercule Poirot e Miss Marple, Agatha Christie é uma das autoras mais vendidas de todos os tempos.",
+        link: "https://pt.wikipedia.org/wiki/Agatha_Christie"
+    },
+    {
+        titulo: "Stephen King",
+        descricao: "Escritor norte-americano, mestre do terror e suspense. Suas obras, como 'O Iluminado' e 'It: A Coisa', são clássicos do gênero e já foram adaptadas para o cinema e televisão.",
+        link: "https://pt.wikipedia.org/wiki/Stephen_King"
+    },
+    {
+        titulo: "Paulo Coelho",
+        descricao: "Escritor brasileiro, conhecido por seus romances que misturam espiritualidade e autoajuda. 'O Alquimista' é uma de suas obras mais famosas e já foi traduzida para diversos idiomas.",
+        link: "https://pt.wikipedia.org/wiki/Paulo_Coelho"
+    }
+];
