72bb2381bab9108ede0053b24e433963ab28acca
Tayni Schukes <taycschukes@gmail.com>
2024-09-07 14:46:22 -0300

    Criando a pesquisa e base de dados

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log(dados);
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+
+    // Itera sobre cada resultado da pesquisa
+    for (let dado of dados) {
+        // Cria um novo elemento HTML para cada resultado
+        resultados += `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Saiba Mais</a>
+            </div>
+        `;
+    }
+
+    // Insere os resultados gerados na seção HTML
+    section.innerHTML = resultados;
+}
