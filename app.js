75ff7b39bd8b193f82fb3a0dd82472de397e4ed7
Tayni Schukes <taycschukes@gmail.com>
2024-09-09 14:49:41 -0300

    Finalizando as aulas Alura

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 function pesquisar() {
     // Seleciona a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
+
+    // Seleciona o elemento que vai fazer a busca e pega apenas o valor
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // Verifica se o campo está vazio
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar algo relacionado aos autores.</p>";
+        return;
+    };
+
+    // Transformando o campo de pesquisa em letra minúscula
+    campoPesquisa = campoPesquisa.toLowerCase();
     
-    // Inicializa uma string vazia para armazenar os resultados
+    // Inicializa strings vazias para armazenar os resultados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada resultado da pesquisa
     for (let dado of dados) {
-        // Cria um novo elemento HTML para cada resultado
-        resultados += `
+        // transforma as variáveis em letra minúscula
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        // Verifica a pesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Cria um novo elemento HTML para cada resultado
+            resultados += `
             <div class="item-resultado">
                 <h2>${dado.titulo}</h2>
                 <p class="descricao-meta">${dado.descricao}</p>
                 <a href=${dado.link} target="_blank">Saiba Mais</a>
             </div>
         `;
+        };
+    };
+
+    // Avisa quando não há resultados da pesquisa
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>";
     }
 
     // Insere os resultados gerados na seção HTML
--- dados.js
+++ dados.js
@@ -2,31 +2,37 @@ let dados = [
     {
         titulo : "William Shakespeare",
         descricao : "O dramaturgo inglês é considerado o autor com o maior número de livros vendidos de todos os tempos, com estimativas que variam entre 2 e 4 bilhões de cópias.",
-        link : "https://pt.wikipedia.org/wiki/William_Shakespeare"
+        link : "https://pt.wikipedia.org/wiki/William_Shakespeare",
+        tags: "fantasma ópera clássico romeu julieta"
     },
     {
         titulo: "J.K. Rowling",
         descricao: "Escritora britânica, mais conhecida por criar o universo de Harry Potter. Suas obras são um fenômeno mundial e inspiraram filmes, peças de teatro e diversos produtos.",
-        link: "https://pt.wikipedia.org/wiki/J._K._Rowling"
+        link: "https://pt.wikipedia.org/wiki/J._K._Rowling",
+        tags: "londres bruxo bruxa trouxa"
     },
     {
         titulo: "Dan Brown",
         descricao: "Autor de best-sellers, Dan Brown é conhecido por suas tramas intrigantes e mistérios que envolvem história, religião e ciência. Seus livros são traduzidos para diversos idiomas e adaptados para o cinema.",
-        link: "https://pt.wikipedia.org/wiki/Dan_Brown"
+        link: "https://pt.wikipedia.org/wiki/Dan_Brown",
+        tags: "código codigo DaVinci inferno dante"
     },
     {
         titulo: "Agatha Christie",
         descricao: "Escritora britânica, considerada a rainha do crime. Conhecida por suas histórias de detetives como Hercule Poirot e Miss Marple, Agatha Christie é uma das autoras mais vendidas de todos os tempos.",
-        link: "https://pt.wikipedia.org/wiki/Agatha_Christie"
+        link: "https://pt.wikipedia.org/wiki/Agatha_Christie",
+        tags: "expresso agata triste casos"
     },
     {
         titulo: "Stephen King",
         descricao: "Escritor norte-americano, mestre do terror e suspense. Suas obras, como 'O Iluminado' e 'It: A Coisa', são clássicos do gênero e já foram adaptadas para o cinema e televisão.",
-        link: "https://pt.wikipedia.org/wiki/Stephen_King"
+        link: "https://pt.wikipedia.org/wiki/Stephen_King",
+        tags: "terror suspense series séries filmes"
     },
     {
         titulo: "Paulo Coelho",
         descricao: "Escritor brasileiro, conhecido por seus romances que misturam espiritualidade e autoajuda. 'O Alquimista' é uma de suas obras mais famosas e já foi traduzida para diversos idiomas.",
-        link: "https://pt.wikipedia.org/wiki/Paulo_Coelho"
+        link: "https://pt.wikipedia.org/wiki/Paulo_Coelho",
+        tags: "brasil nacional"
     }
 ];
